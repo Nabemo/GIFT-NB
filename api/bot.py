a08dbf4f24524220e0609fc3eeff97b7231db034
Nabemo <61938094+Nabemo@users.noreply.github.com>
2025-08-05 21:56:31 +0300

    Add files via upload

--- api/bot.py
+++ api/bot.py
@@ -1,15 +1,16 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
+import os
 
-bot = Bot(token=os.getenv("68d1d2e5c4734ed3bb788343320071b2"))
+bot = Bot(token=os.getenv("7833462753:AAHFvHWIT18D8CzSYA5Vqg1IBbPllcsV6gE"))
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.reply("Бот активен! Мониторим NFT...")
+    await message.reply("🔍 Бот активен! Мониторим NFT-подарки в Tonnel...")
 
 async def on_startup(dp):
-    await bot.set_webhook("https://gift-nb-nabemo53s-projects.vercel.app/")
+    await bot.set_webhook("https://gift-nb-nabemo53s-projects.vercel.app/webhook")
 
 if __name__ == '__main__':
     executor.start_webhook(
@@ -18,3 +19,4 @@ if __name__ == '__main__':
         on_startup=on_startup,
         host='0.0.0.0',
         port=int(os.environ.get('PORT', 5000))
+    )
